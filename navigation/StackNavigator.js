e3ba8d9e056f07e050a3df4d9412a271b598f06e
Desire <desireirankundawilliams@gmail.com>
2023-09-27 17:13:38 +0200

    added login and register screens

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
